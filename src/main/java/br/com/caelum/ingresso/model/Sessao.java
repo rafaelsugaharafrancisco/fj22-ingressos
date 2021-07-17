ea4ebab1ebefd89ff208912de302177c3e0877e4
Rafael Francisco <rafa.s.francisco@gmail.com>
2021-07-17 12:02:04 -0300

    incluindo preco nas classes: sala, filme e sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -14,6 +16,7 @@ public class Sessao {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	private BigDecimal preco;
 	
 	@ManyToOne
 	private Sala sala;
@@ -27,6 +30,7 @@ public class Sessao {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public Integer getId() {
@@ -43,6 +47,14 @@ public class Sessao {
 
 	public void setHorario(LocalTime horario) {
 		this.horario = horario;
+	}
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 
 	public Sala getSala() {
